74dd0fb975c1457c4c9d7da9a1e73c4d4ab26b0b
WangariGikonyo <wangarie0@gmail.com>
2024-03-03 20:47:46 +0300

    build a cart component which has a saerchbar, shopping cart and checkout button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 // import './App.css'
 import Navbar from './components/navbar/Navbar'
 import { Hero } from './components/hero/Hero'
+import { Cart } from './components/cart/Cart'
 
 function App() {
   return (
    <div>
     <Navbar />
     <Hero />
+    <Cart />
     
    </div>
   )
---
+++ frontend/src/components/cart/Cart.jsx
@@ -0,0 +1,30 @@
+import './Cart.css'
+
+export function Cart() {
+  return (
+    <div className="  container search-bar ">  
+    <div className="row">
+        <div class="col-md-6 search-col">
+            <input type="search"
+            placeholder="Search products"
+            />
+             <i class="fa fa-search"/>
+        </div>
+        <div class="  col-md-6 cart-col">
+            <div>
+        <i class="fa fa-shopping-cart" aria-hidden="true"></i> 0 items in your cart
+
+
+            </div>
+        
+        <button>Checkout</button>
+
+
+        </div>
+    
+
+    </div>
+    
+    </div>
+  );
+}
